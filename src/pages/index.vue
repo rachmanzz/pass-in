76a8627cd5a123cbd0309466baf08adce6d6cecd
Rahman <rachman.sd@gmail.com>
2018-08-07 16:54:03 +0800

    add vuex & Reqres.in res api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
             <v-select
             :items="reqmethod"
             label="Method"
-            v-model="formassets.method"
+            v-model="getMethod"
             height="40"
             color="info"
             outline
@@ -13,7 +13,7 @@
         </v-flex>
         <v-flex xs7 sm10 pl-2 d-flex>
             <v-text-field
-                v-model="url"
+                v-model="getUrl"
                 label="URL"
                 height="40"
                 outline
@@ -25,7 +25,6 @@
     </v-layout>
     <v-layout row wrap>
         <v-flex xs12 sm12 d-flex>
-            
             <v-tabs
              color="cyan"
              dark
@@ -93,7 +92,7 @@
                             </v-flex>
                         </v-layout>
                     </v-card>
-                    <v-card  v-show="n.code === 3 && formassets.method === 'POST'" style="padding:10px">
+                    <v-card  v-show="n.code === 3 && formassets.method !== 'GET' && formassets.method !== 'DELETE'" style="padding:10px">
                         <v-layout row wrap v-for="(item, i) in formassets.body" :key="i">
                             <v-flex xs4 sm3 d-flex>
                                 <v-text-field
@@ -122,7 +121,7 @@
                     </v-card>
                     <v-card  v-show="n.code === 4" style="padding:10px">
                         <h3>Form Request</h3>
-                        <v-data-table :items="[{ name: formassets.method, val: url }]" :headers="[ { text: 'Method', value: 'Method' }, { text: 'Url', value: 'Url' } ]">
+                        <v-data-table :items="[{ name: formassets.method, val: getUrl }]" :headers="[ { text: 'Method', value: 'Method' }, { text: 'Url', value: 'Url' } ]">
                           <template slot="items" slot-scope="props">
                               <td>{{props.item.name}}</td>
                               <td>{{props.item.val}}</td>
@@ -145,7 +144,7 @@
                           </template>
                         </v-data-table>
                         <hr>
-                        <h3 v-show="formassets.method === 'POST'">Body</h3>
+                        <h3 v-show="formassets.method !== 'GET' && formassets.method !== 'DELETE'">Body</h3>
                         <v-data-table :items="formassets.body" v-show="formassets.method === 'POST'" :headers="[ { text: 'name', value: 'name' }, { text: 'value', align: 'left', value: 'value' } ]">
                           <template slot="items" slot-scope="props">
                               <td>{{props.item.name}}</td>
@@ -157,6 +156,14 @@
                       Response Status : {{result.status}}
                       <hr>
                       <vue-json-pretty :path="'res'" :data="result.data"></vue-json-pretty>
+                    </v-card>
+                    <v-card v-show="n.code === 6" style="padding:10px">
+                      Response Status : {{ errors.code }}
+                      <hr>
+                      <div v-html="errors.data"></div>
+                      <hr>
+                      <h3>Headers</h3>
+                      <vue-json-pretty :path="'res'" :data="errors.headers"></vue-json-pretty>
                     </v-card>
                   </v-tab-item>
             </v-tabs>
@@ -223,9 +230,17 @@ export default {
       ],
       reqmethod: [
         'GET',
-        'POST'
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE'
       ],
-      url: '',
+      result: {},
+      errors: {
+        code: 500,
+        data: '',
+        headers: {}
+      },
       formassets: {
         method: 'GET',
         headers: [
@@ -237,13 +252,33 @@ export default {
         body: [
           { name: '', val: '' }
         ]
+      }
+    }
+  },
+  mounted () {
+    this.formassets = this.$store.state.formassets
+  },
+  computed: {
+    getMethod: {
+      set (value) {
+        this.$store.commit('setMethod', value)
       },
-      result: {}
+      get () {
+        return this.$store.state.formassets.method
+      }
+    },
+    getUrl: {
+      set (value) {
+        this.$store.commit('setUri', value)
+      },
+      get () {
+        return this.$store.state.url
+      }
     }
   },
   methods: {
     generateHeaders () {
-      const data = this.formassets.headers
+      const data = this.$store.state.formassets.headers
       const size = data.length
       const result = { able: false, data: {} }
       for (let i = 0; i < size; i++) {
@@ -256,7 +291,7 @@ export default {
       return result
     },
     generateParams () {
-      const data = this.formassets.params
+      const data = this.$store.state.formassets.params
       const size = data.length
       const result = { able: false, data: {} }
       for (let i = 0; i < size; i++) {
@@ -269,7 +304,7 @@ export default {
       return result
     },
     generateBody () {
-      const data = this.formassets.body
+      const data = this.$store.state.formassets.body
       const size = data.length
       const result = { able: false, data: {} }
       for (let i = 0; i < size; i++) {
@@ -281,64 +316,98 @@ export default {
       }
       return result
     },
+    removeTabs (code) {
+      const index = this.indexOfCode(this.tabsitem, code)
+      if (index >= 0) {
+        this.tabsitem.splice(index, 1)
+      }
+    },
+    indexOfCode (arr, looking) {
+      const res = arr.map(e => { return e.code })
+      return res.indexOf(looking)
+    },
     headersAction (i) {
-      const size = this.formassets.headers.length - 1
+      const size = this.$store.state.formassets.headers.length - 1
       if (size > i) {
-        this.formassets.headers.splice(i, 1)
+        this.$store.commit('removeHeader', 1)
       } else {
-        this.formassets.headers.push({ name: '', val: '' })
+        this.$store.commit('setHeader', { name: '', val: '' })
       }
     },
     bodyAction (i) {
-      const size = this.formassets.body.length - 1
+      const size = this.$store.state.formassets.body.length - 1
       if (size > i) {
-        this.formassets.body.splice(i, 1)
+        this.$store.commit('removeBody', 1)
       } else {
-        this.formassets.body.push({ name: '', val: '' })
+        this.$store.commit('setBody', { name: '', val: '' })
       }
     },
     paramsAction (i) {
-      const size = this.formassets.params.length - 1
+      const size = this.$store.state.formassets.params.length - 1
       if (size > i) {
-        this.formassets.params.splice(i, 1)
+        this.$store.commit('removeParam', 1)
       } else {
-        this.formassets.params.push({ name: '', val: '' })
+        this.$store.commit('setParam', { name: '', val: '' })
       }
     },
     async sendAction () {
+      this.removeTabs(4)
+      this.removeTabs(5)
+      this.removeTabs(6)
       this.mdialog = false
       if (this.url === '') {
         alert('error, url empty')
         return
       }
-      this.tabsitem.push({ name: 'Send', code: 4 })
+      if (this.indexOfCode(this.tabsitem, 4) < 0) this.tabsitem.push({ name: 'Send', code: 4 })
       this.loading = true
-      const method = this.formassets.method.toLowerCase()
+      const method = this.$store.state.formassets.method.toLowerCase()
       const arg = {
         method: method,
-        url: this.url
+        url: this.$store.state.url
       }
       const mheaders = this.generateHeaders()
       const mparams = this.generateParams()
       const mbodies = this.generateBody()
+
       if (mheaders.able) {
         arg.headers = mheaders.data
+        arg.headers['Access-Control-Expose-Headers'] = 'error'
+      } else {
+        arg.headers = {
+          'Access-Control-Expose-Headers': 'error'
+        }
       }
       if (mparams.able) {
         arg.params = mparams.data
       }
-      if (method === 'post' && mbodies.able) {
+      if (method !== 'get' && method !== 'delete' && mbodies.able) {
         arg.data = mbodies.data
       }
       try {
         const response = await axios(arg)
         if (response) {
-          this.tabsitem.push({ name: 'Result', code: 5 })
-          this.loading = false
+          if (this.indexOfCode(this.tabsitem, 5) < 0) this.tabsitem.push({ name: 'Result', code: 5 })
           this.result = response
+        } else {
+          if (this.indexOfCode(this.tabsitem, 6) < 0) this.tabsitem.push({ name: 'Error', code: 6 })
+          this.errors.code = response.status
+          this.errors.data = response.data
+          this.errors.headers = response.headers
         }
-      } catch (err) {
-        console.log(err)
+        this.loading = false
+      } catch (error) {
+        if (typeof error.response !== 'undefined') {
+          this.errors.code = error.response.status
+          this.errors.data = error.response.data
+          this.errors.headers = error.response.headers
+        } else {
+          this.errors.data = error.message
+          this.errors.headers = error.config.headers
+          this.errors.code = error.status
+        }
+
+        if (this.indexOfCode(this.tabsitem, 6) < 0) this.tabsitem.push({ name: 'Error', code: 6 })
         this.loading = false
       }
     }
